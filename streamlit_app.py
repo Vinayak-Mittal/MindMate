f68f6809305c487eb96c3e40fa3217dd6e3b787f
Vinayak Mittal <116906037+Vinayak-Mittal@users.noreply.github.com>
2025-01-30 10:21:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ def get_base64(background):
         data = f.read()
     return base64.b64encode(data).decode()
 
-bin_str = get_base64("background.png")
+bin_str = get_base64("path/to/background.png")
 
 
 st.markdown(f"""
